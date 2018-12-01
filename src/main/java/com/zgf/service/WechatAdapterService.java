501c01a09b0f7edc1661369ec6e3ab2a1d86c1d0
zgf <chriszh123@sina.com>
2018-12-01 15:16:33 +0800

    登录部分请求接口

---
+++ src/main/java/com/zgf/service/WechatAdapterService.java
@@ -0,0 +1,4 @@
+package com.zgf.service;
+
+public interface WechatAdapterService {
+}
---
+++ src/main/java/com/zgf/service/impl/WechatAdapterServiceImpl.java
@@ -0,0 +1,12 @@
+package com.zgf.service.impl;
+
+import com.zgf.service.WechatAdapterService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service("wechatAdapterService")
+public class WechatAdapterServiceImpl implements WechatAdapterService {
+    private final Logger logger = LoggerFactory.getLogger(WechatAdapterServiceImpl.class);
+
+}

a3aa5fba94b8a4166292f049c5958458989c11f6
zgf <chriszh123@sina.com>
2018-12-02 22:19:01 +0800

    登录部分请求接口

---
+++ src/main/java/com/zgf/service/UserService.java
@@ -0,0 +1,12 @@
+package com.zgf.service;
+
+import com.zgf.model.User;
+
+/**
+ * Created by zgf
+ * Date 2018/12/2 22:02
+ * Description
+ */
+public interface UserService {
+    void saveOrUpdate(User user);
+}
